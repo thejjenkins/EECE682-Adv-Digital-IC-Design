72a478fde26e1a50223b29def1e4d48095384750
Jeff Mofor <allojeff444@gmail.com>
2026-02-23 22:21:05 -0500

    Add testbench

---
+++ tb/rk4_uart_sequencer.sv
@@ -0,0 +1,4 @@
+// =============================================================================
+//  rk4_uart_sequencer.sv -- UVM sequencer for UART TX items
+// =============================================================================
+typedef uvm_sequencer #(rk4_uart_tx_item) rk4_uart_sequencer;
